aec197ba94745e86a599054503e4f0158def0bf0
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-06-06 09:20:11 +0200

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
+import * as path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		outDir: "dist",
-		rollupOptions: {
-			input: "src/index.ts",
-		},
-	},
-})
+								plugins: [react()],
+								build: {
+									outDir: 'dist',
+									lib: {
+										entry: path.resolve(__dirname, 'src/index.tsx'),
+										name: 'MyLibrary'
+									},
+									rollupOptions: {
+										output: {
+											entryFileNames: '[name].js' // add this line
+										}
+									}
+								}
+							})
